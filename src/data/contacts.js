f4b2ffa474f1eeb21d8a97bdf2e3111e357d532e
thomas0114 <thomas.k98114@gmail.com>
2023-12-11 01:46:42 -0500

    first commit

---
+++ src/data/contacts.js
@@ -0,0 +1,40 @@
+import {
+  FaTelegramPlane,
+  FaTwitter,
+  FaDiscord,
+  FaGithub,
+  FaMedium,
+} from "react-icons/fa";
+
+export const dataContacts = [
+  {
+    icon: <FaTelegramPlane />,
+    colorFont: "white",
+    colorBack: "#5E9EFE",
+    urlContact: "https://t.me/Willow_Token",
+  },
+  {
+    icon: <FaTwitter />,
+    colorFont: "white",
+    colorBack: "black",
+    urlContact: "https://t.me/Willow_Token",
+  },
+  {
+    icon: <FaDiscord />,
+    colorFont: "white",
+    colorBack: "#6201FF",
+    urlContact: "https://t.me/Willow_Token",
+  },
+  {
+    icon: <FaGithub />,
+    colorFont: "white",
+    colorBack: "black",
+    urlContact: "https://t.me/Willow_Token",
+  },
+  {
+    icon: <FaMedium />,
+    colorFont: "white",
+    colorBack: "#30F",
+    urlContact: "https://t.me/Willow_Token",
+  },
+];
